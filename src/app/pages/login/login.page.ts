857d238b83d5737be493ce8d95f048479944e5a2
juan.ortiz <juan.ortizf@infotec.mx>
2025-03-15 08:31:40 -0600

    ajuste labels

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { IonContent, IonInput, IonRow, IonGrid, IonCol, IonButton, IonItem, IonCard,IonCardContent, IonFooter, IonToolbar,
+import { IonContent, IonInput, IonRow, IonGrid, IonCol, IonButton, IonItem, IonCard,IonCardContent, IonText,
   IonCardHeader, IonCardTitle, IonRouterLink, IonCardSubtitle } from '@ionic/angular/standalone';
 import { FooterComponent } from "../../layout/footer/footer.component";
 import { HeaderComponent } from "../../layout/header/header.component";
@@ -10,15 +10,14 @@ import { Router, RouterModule } from '@angular/router';
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  imports: [IonContent, IonInput, IonRow, IonGrid, IonCol, IonButton, IonItem, IonCard, IonCardContent, IonCardSubtitle,
+  imports: [IonContent, IonInput, IonRow, IonGrid, IonCol, IonButton, IonItem, IonCard, IonCardContent, IonCardSubtitle, IonText,
     IonCardHeader, IonCardTitle,  FooterComponent, HeaderComponent, NavbarComponent,
     RouterModule, IonRouterLink],
 })
 export class LoginPage implements OnInit {
   public folder!: string;
-  @ViewChild('contPie', { static: false }) pie!: ElementRef;
 
-  constructor(private router: Router) {}
+  constructor() {}
 
   ngOnInit() {
     this.folder = 'SIGAM';
