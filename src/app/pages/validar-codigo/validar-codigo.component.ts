5c684d03ef031cbdac8cde2c32c2cac48e1137c4
juan.ortiz <juan.ortizf@infotec.mx>
2025-03-18 12:04:20 -0600

    se agregan terminos y condiciones

--- src/app/pages/validar-codigo/validar-codigo.component.ts
+++ src/app/pages/validar-codigo/validar-codigo.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { IonContent, IonInput, IonItem,  IonButton, IonList, IonRouterLink, } from '@ionic/angular/standalone';
+import { IonContent, IonInput, IonItem,  IonButton, IonList, IonRouterLink, IonLabel, IonToolbar, IonToggle, IonButtons, IonTitle, IonModal, IonHeader } from '@ionic/angular/standalone';
 import { FooterComponent } from 'src/app/layout/footer/footer.component';
 import { HeaderComponent } from 'src/app/layout/header/header.component';
 
@@ -8,11 +8,13 @@ import { HeaderComponent } from 'src/app/layout/header/header.component';
   selector: 'app-validar-codigo',
   templateUrl: './validar-codigo.component.html',
   styleUrls: ['./validar-codigo.component.scss'],
-   imports: [HeaderComponent, IonContent, IonItem, IonButton, IonList,
+   imports: [IonHeader, IonModal, IonTitle, IonButtons, IonToggle, IonLabel, HeaderComponent, IonContent, IonItem, IonButton, IonList, IonToolbar,
       IonRouterLink, RouterModule, IonInput, FooterComponent],
 })
 export class ValidarCodigoComponent  implements OnInit {
   public tipoRegistro: string = '';
+  public terminosAceptados = false;
+  @ViewChild(IonModal) modal!: IonModal;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -21,6 +23,18 @@ export class ValidarCodigoComponent  implements OnInit {
       this.tipoRegistro = params.tipoRegistro;
       console.log(this.tipoRegistro)
     });
+  }
+
+  abrirTerminos() {
+    this.terminosAceptados = true;
+  }
+
+  cancelar() {
+    this.modal.dismiss(null, 'Cancelar');
+  }
+
+  aceptar() {
+    this.modal.dismiss(null, 'Aceptar');
   }
 
 }
